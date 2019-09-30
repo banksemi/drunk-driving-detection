d32f04431c8beb349eab28d5d9b6c471d7bbc02a
Seunghwa <banksemi@naver.com>
2019-09-30 17:11:47 +0900

    Add sample project (tizen, android)

---
+++ Test Project/Provider(Tizen)_Consumer(Android)/Consumer/app/src/main/java/com/samsung/android/sdk/accessory/example/helloaccessory/consumer/ConsumerService.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved. 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
+ * the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice, 
+ *       this list of conditions and the following disclaimer. 
+ *     * Redistributions in binary form must reproduce the above copyright notice, 
+ *       this list of conditions and the following disclaimer in the documentation and/or 
+ *       other materials provided with the distribution. 
+ *     * Neither the name of Samsung Electronics Co., Ltd. nor the names of its contributors may be used to endorse or 
+ *       promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.samsung.android.sdk.accessory.example.helloaccessory.consumer;
+
+import java.io.IOException;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+import android.util.Log;
+
+import com.samsung.android.sdk.SsdkUnsupportedException;
+import com.samsung.android.sdk.accessory.*;
+
+public class ConsumerService extends SAAgent {
+    private static final String TAG = "HelloAccessory(C)";
+    private static final Class<ServiceConnection> SASOCKET_CLASS = ServiceConnection.class;
+    private final IBinder mBinder = new LocalBinder();
+    private ServiceConnection mConnectionHandler = null;
+    Handler mHandler = new Handler();
+
+    public ConsumerService() {
+        super(TAG, SASOCKET_CLASS);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SA mAccessory = new SA();
+        try {
+            mAccessory.initialize(this);
+        } catch (SsdkUnsupportedException e) {
+            // try to handle SsdkUnsupportedException
+            if (processUnsupportedException(e) == true) {
+                return;
+            }
+        } catch (Exception e1) {
+            e1.printStackTrace();
+            /*
+             * Your application can not use Samsung Accessory SDK. Your application should work smoothly
+             * without using this SDK, or you may want to notify user and close your application gracefully
+             * (release resources, stop Service threads, close UI thread, etc.)
+             */
+            stopSelf();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    protected void onFindPeerAgentsResponse(SAPeerAgent[] peerAgents, int result) {
+        if ((result == SAAgent.PEER_AGENT_FOUND) && (peerAgents != null)) {
+            for(SAPeerAgent peerAgent:peerAgents)
+                requestServiceConnection(peerAgent);
+        } else if (result == SAAgent.FINDPEER_DEVICE_NOT_CONNECTED) {
+            Toast.makeText(getApplicationContext(), "FINDPEER_DEVICE_NOT_CONNECTED", Toast.LENGTH_LONG).show();
+            updateTextView("Disconnected");
+        } else if (result == SAAgent.FINDPEER_SERVICE_NOT_FOUND) {
+            Toast.makeText(getApplicationContext(), "FINDPEER_SERVICE_NOT_FOUND", Toast.LENGTH_LONG).show();
+            updateTextView("Disconnected");
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.NoPeersFound, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onServiceConnectionRequested(SAPeerAgent peerAgent) {
+        if (peerAgent != null) {
+            acceptServiceConnectionRequest(peerAgent);
+        }
+    }
+
+    @Override
+    protected void onServiceConnectionResponse(SAPeerAgent peerAgent, SASocket socket, int result) {
+        if (result == SAAgent.CONNECTION_SUCCESS) {
+            this.mConnectionHandler = (ServiceConnection) socket;
+            updateTextView("Connected");
+        } else if (result == SAAgent.CONNECTION_ALREADY_EXIST) {
+            updateTextView("Connected");
+            Toast.makeText(getBaseContext(), "CONNECTION_ALREADY_EXIST", Toast.LENGTH_LONG).show();
+        } else if (result == SAAgent.CONNECTION_DUPLICATE_REQUEST) {
+            Toast.makeText(getBaseContext(), "CONNECTION_DUPLICATE_REQUEST", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getBaseContext(), R.string.ConnectionFailure, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onError(SAPeerAgent peerAgent, String errorMessage, int errorCode) {
+        super.onError(peerAgent, errorMessage, errorCode);
+    }
+
+    @Override
+    protected void onPeerAgentsUpdated(SAPeerAgent[] peerAgents, int result) {
+        final SAPeerAgent[] peers = peerAgents;
+        final int status = result;
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (peers != null) {
+                    if (status == SAAgent.PEER_AGENT_AVAILABLE) {
+                        Toast.makeText(getApplicationContext(), "PEER_AGENT_AVAILABLE", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "PEER_AGENT_UNAVAILABLE", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    public class ServiceConnection extends SASocket {
+        public ServiceConnection() {
+            super(ServiceConnection.class.getName());
+        }
+
+        @Override
+        public void onError(int channelId, String errorMessage, int errorCode) {
+        }
+
+        @Override
+        public void onReceive(int channelId, byte[] data) {
+            final String message = new String(data);
+            addMessage("Received: ", message);
+        }
+
+        @Override
+        protected void onServiceConnectionLost(int reason) {
+            updateTextView("Disconnected");
+            closeConnection();
+        }
+    }
+
+    public class LocalBinder extends Binder {
+        public ConsumerService getService() {
+            return ConsumerService.this;
+        }
+    }
+
+    public void findPeers() {
+        findPeerAgents();
+    }
+
+    public boolean sendData(final String data) {
+        boolean retvalue = false;
+        if (mConnectionHandler != null) {
+            try {
+                mConnectionHandler.send(getServiceChannelId(0), data.getBytes());
+                retvalue = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            addMessage("Sent: ", data);
+        }
+        return retvalue;
+    }
+
+    public boolean closeConnection() {
+        if (mConnectionHandler != null) {
+            mConnectionHandler.close();
+            mConnectionHandler = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean processUnsupportedException(SsdkUnsupportedException e) {
+        e.printStackTrace();
+        int errType = e.getType();
+        if (errType == SsdkUnsupportedException.VENDOR_NOT_SUPPORTED
+                || errType == SsdkUnsupportedException.DEVICE_NOT_SUPPORTED) {
+            /*
+             * Your application can not use Samsung Accessory SDK. You application should work smoothly
+             * without using this SDK, or you may want to notify user and close your app gracefully (release
+             * resources, stop Service threads, close UI thread, etc.)
+             */
+            stopSelf();
+        } else if (errType == SsdkUnsupportedException.LIBRARY_NOT_INSTALLED) {
+            Log.e(TAG, "You need to install Samsung Accessory SDK to use this application.");
+        } else if (errType == SsdkUnsupportedException.LIBRARY_UPDATE_IS_REQUIRED) {
+            Log.e(TAG, "You need to update Samsung Accessory SDK to use this application.");
+        } else if (errType == SsdkUnsupportedException.LIBRARY_UPDATE_IS_RECOMMENDED) {
+            Log.e(TAG, "We recommend that you update your Samsung Accessory SDK before using this application.");
+            return false;
+        }
+        return true;
+    }
+
+    private void updateTextView(final String str) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                ConsumerActivity.updateTextView(str);
+            }
+        });
+    }
+
+    private void addMessage(final String prefix, final String data) {
+        final String strToUI = prefix.concat(data);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                ConsumerActivity.addMessage(strToUI);
+            }
+        });
+    }
+}
---
+++ Test Project/Provider(Tizen)_Consumer(Android)/Provider/js/app.js
@@ -0,0 +1,161 @@
+/*
+* Copyright (c) 2015 Samsung Electronics Co., Ltd.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* * Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following disclaimer
+* in the documentation and/or other materials provided with the
+* distribution.
+* * Neither the name of Samsung Electronics Co., Ltd. nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var SAAgent,
+    SASocket,
+    connectionListener,
+    responseTxt = document.getElementById("responseTxt");
+
+/* Make Provider application running in background */
+tizen.application.getCurrentApplication().hide();
+
+function createHTML(log_string)
+{
+    var content = document.getElementById("toast-content");
+    content.innerHTML = log_string;
+    tau.openPopup("#toast");
+}
+
+connectionListener = {
+    /* Remote peer agent (Consumer) requests a service (Provider) connection */
+    onrequest: function (peerAgent) {
+
+        createHTML("peerAgent: peerAgent.appName<br />" +
+                    "is requsting Service conncetion...");
+
+        /* Check connecting peer by appName*/
+        if (peerAgent.appName === "HelloAccessoryConsumer") {
+            SAAgent.acceptServiceConnectionRequest(peerAgent);
+            createHTML("Service connection request accepted.");
+
+        } else {
+            SAAgent.rejectServiceConnectionRequest(peerAgent);
+            createHTML("Service connection request rejected.");
+
+        }
+    },
+
+    /* Connection between Provider and Consumer is established */
+    onconnect: function (socket) {
+        var onConnectionLost,
+            dataOnReceive;
+
+        createHTML("Service connection established");
+
+        /* Obtaining socket */
+        SASocket = socket;
+
+        onConnectionLost = function onConnectionLost (reason) {
+            createHTML("Service Connection disconnected due to following reason:<br />" + reason);
+        };
+
+        /* Inform when connection would get lost */
+        SASocket.setSocketStatusListener(onConnectionLost);
+
+        dataOnReceive =  function dataOnReceive (channelId, data) {
+            var newData;
+
+            if (!SAAgent.channelIds[0]) {
+                createHTML("Something goes wrong...NO CHANNEL ID!");
+                return;
+            }
+            newData = data + " :: " + new Date();
+
+            /* Send new data to Consumer */
+            SASocket.sendData(SAAgent.channelIds[0], newData);
+            createHTML("Send massage:<br />" +
+                        newData);
+        };
+
+        /* Set listener for incoming data from Consumer */
+        SASocket.setDataReceiveListener(dataOnReceive);
+    },
+    onerror: function (errorCode) {
+        createHTML("Service connection error<br />errorCode: " + errorCode);
+    }
+};
+
+function requestOnSuccess (agents) {
+    var i = 0;
+
+    for (i; i < agents.length; i += 1) {
+        if (agents[i].role === "PROVIDER") {
+            createHTML("Service Provider found!<br />" +
+                        "Name: " +  agents[i].name);
+            SAAgent = agents[i];
+            break;
+        }
+    }
+
+    /* Set listener for upcoming connection from Consumer */
+    SAAgent.setServiceConnectionListener(connectionListener);
+};
+
+function requestOnError (e) {
+    createHTML("requestSAAgent Error" +
+                "Error name : " + e.name + "<br />" +
+                "Error message : " + e.message);
+};
+
+/* Requests the SAAgent specified in the Accessory Service Profile */
+webapis.sa.requestSAAgent(requestOnSuccess, requestOnError);
+
+
+(function () {
+    /* Basic Gear gesture & buttons handler */
+    window.addEventListener('tizenhwkey', function(ev) {
+        var page,
+            pageid;
+
+        if (ev.keyName === "back") {
+            page = document.getElementsByClassName('ui-page-active')[0];
+            pageid = page ? page.id : "";
+            if (pageid === "main") {
+                try {
+                    tizen.application.getCurrentApplication().exit();
+                } catch (ignore) {
+                }
+            } else {
+                window.history.back();
+            }
+        }
+    });
+}());
+
+(function(tau) {
+    var toastPopup = document.getElementById('toast');
+
+    toastPopup.addEventListener('popupshow', function(ev){
+        setTimeout(function () {
+            tau.closePopup();
+        }, 3000);
+    }, false);
+})(window.tau);
