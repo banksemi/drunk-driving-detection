07c44e3a70926c49308fb87ff6c08c87415d6c4f
seunghwa <banksemi@naver.com>
2019-11-28 05:14:12 +0900

    Fix random UUID

--- Test Project/Provider(Tizen)_Consumer(Android)/Consumer/app/src/main/java/com/samsung/android/sdk/accessory/example/helloaccessory/consumer/UUIDManager.java
+++ Test Project/Provider(Tizen)_Consumer(Android)/Consumer/app/src/main/java/com/samsung/android/sdk/accessory/example/helloaccessory/consumer/UUIDManager.java
@@ -1,12 +1,26 @@
 package com.samsung.android.sdk.accessory.example.helloaccessory.consumer;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import java.util.UUID;
 
 public  class UUIDManager {
-    public static String GetDevicesUUID(Context mContext){
-        String uniqueID = UUID.randomUUID().toString();
+    private static String uniqueID = null;
+    private static final String PREF_UNIQUE_ID = "PREF_UNIQUE_ID";
+
+    public static String GetDevicesUUID(Context context){
+        if (uniqueID == null) {
+            SharedPreferences sharedPrefs = context.getSharedPreferences(
+                    PREF_UNIQUE_ID, Context.MODE_PRIVATE);
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null);
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString(PREF_UNIQUE_ID, uniqueID);
+                editor.commit();
+            }
+        }
         return uniqueID;
     }
 }
